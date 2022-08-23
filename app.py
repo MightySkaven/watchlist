edd6490ebc05dc365ecce01c34db2e4607cb70c1
lyy <1053059831@qq.com>
2022-08-23 21:46:58 +0800

    add database with flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
 from flask import Flask, render_template
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+import click
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+# 自动化初始数据库操作
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 
 name = 'Yuanyi Liu'
 movies = [
@@ -16,13 +49,26 @@ movies = [
     {'title': 'WALL-E', 'year': '2008'},
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
+@app.cli.command()
+def forge():
+    """generate fake data."""
+    db.create_all()
+    user=User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done')
 @app.route('/')  #一个视图函数可以绑定多个URL,通过附加多个装饰器来实现
 @app.route('/index')
 @app.route('/home')
 # def hello():
 #     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
 @app.route('/user/<name>')  #可以在URL中定义变量部分，下面的函数会处理所有/user/name的请求
 def user_page(name):
     return 'User page'
